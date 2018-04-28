66bd964850e6e3f676bfcd49412e3014f7388cf2
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2018-04-28 14:01:55 +0200

    Add ability to encrypt and decrypt data using password

---
+++ src/App.encryption.js
@@ -0,0 +1,40 @@
+import openpgp, { message, util } from 'openpgp';
+
+openpgp.config = {aead_protect: true};
+
+export class Encryptor {
+  static setPassword(password) {
+    localStorage.setItem('encryption-password', password);
+  }
+
+  static hasEncryptionPassword() {
+    return this.getPassword() !== null;
+  }
+
+  static getPassword() {
+    return localStorage.getItem('encryption-password');
+  }
+
+  static async encrypt(text) {
+    const password = this.getPassword();
+    const encrypted = await openpgp.encrypt({
+      data: text,
+      passwords: [password],
+      armor: false,
+    });
+    const message = encrypted.message.packets.write();
+
+    return util.Uint8Array_to_b64(message);
+  }
+
+  static async decrypt(ciphertext) {
+    const password = this.getPassword();
+    const sourceArray = util.b64_to_Uint8Array(ciphertext);
+    const plaintext = await openpgp.decrypt({
+      message: message.read(sourceArray),
+      passwords: [password],
+    });
+
+    return plaintext.data;
+  }
+}
